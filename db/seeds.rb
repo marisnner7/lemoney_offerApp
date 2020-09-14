ed300359c19f2e383f6e54b4270c872ebdc58e90
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-09-13 23:40:14 -0300

    first seed for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+nicole = User.create!({  email: "nicole@gmail.com", password: "1234567", admin: true})
+milene = User.create!({ email: "milene@gmail.com", password: "1234567", admin: true })
+
+
+
+orange_juice = Offer.create!({ advertiser_name: "Orange Juice", description: "Organic Oranges from Ecuador", url: "https://www.google.com/", starts_at: "05/10/2020", user_id: nicole.id})
+
+coffee = Offer.create!({ advertiser_name: "Cofee Roasters", description: "Organic Blend. All natural and unfiltered", url: "https://www.google.com/", starts_at: "05/10/2020", user_id: milene.id})
