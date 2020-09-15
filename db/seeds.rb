09b36dfca2826ec5c848cd3c481c9592d79a527b
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-09-15 01:06:02 -0300

    great seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts "destroy all offers"
+Offer.destroy_all
+puts "destroy all users"
+User.destroy_all
+
+puts 'creating a admin'
 admin = User.create!(email:"admin@example.com", password:"123456", admin: true)
 
+puts 'creating 15 offers without ends_at date'
+15.times do
+  offer = Offer.new(advertiser_name: Faker::Company.name, url: Faker::Internet.url,
+    description: Faker::Lorem.sentence,
+    starts_at: Faker::Date.between(from: 2.days.ago, to: 5.days.from_now),
+    disable: Random.rand > 0.7, premium: Random.rand > 0.8, user: admin)
+  
+  offer.save!
+end
 
-Offer.create!( advertiser_name: "Garmin Forerunner 235", description: "GPS running watch with wrist based heart rate and display type is sunlight visible", url: "https://www.google.com/", starts_at: "05/10/2020", user: admin)
+puts "creating 10 offers with ends_at date"
+10.times do
+  offer = Offer.new(advertiser_name: Faker::Company.name, url: Faker::Internet.url,
+    description: Faker::Lorem.sentence,
+    disable: Random.rand > 0.7, premium: Random.rand > 0.8, user: admin,
+    starts_at: Faker::Date.between(from: 2.days.ago, to: 5.days.from_now),
+    ends_at: Faker::Date.between(from: 5.days.from_now, to: 10.days.from_now))
+  
+  offer.save!
+end
 
-Offer.create!( advertiser_name: "Cofee Roasters", description: "Organic Blend. All natural and unfiltered", url: "https://www.google.com/", starts_at: "05/12/2020", user: admin)
