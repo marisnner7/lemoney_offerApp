c65bb5784f58bb8aeb287934fa06b3e29b7e30ac
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-09-15 02:54:05 -0300

    tests for offer model

--- spec/models/offer_spec.rb
+++ spec/models/offer_spec.rb
@@ -1,15 +1,17 @@
-RSpec.describe Offer do
-  subject { offer }
-  let(:offer) { FactoryBot.build :offer }
+require 'rails_helper'
 
-  it { is_expected.to be_valid }
-
+RSpec.describe Offer, type: :model do
   describe 'Validations' do
-    it { is_expected.to validate_presence_of :advertiser_name }
-    it { is_expected.to validate_uniqueness_of :advertiser_name }
-    it { is_expected.to validate_presence_of :url }
-    it { is_expected.to validate_presence_of :description }
-    it { is_expected.to validate_presence_of :starts_at }
+    it { should validate_presence_of(:url) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:starts_at) }
   end
 
-end  
+  describe 'Scopes' do
+    describe '#enabled' do
+      subject { Offer.enabled }
+
+      it { is_expected.to be_an ActiveRecord::Relation }
+    end
+  end
+end
