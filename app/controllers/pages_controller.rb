02d6e933fadd00c136b9a60836c535fa502e708c
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-09-15 01:06:45 -0300

    calling offers by premium or remaining_time

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    #@offers = Offer.all
+    #@offers = Offer.where(state: :enabled).order(premium: :desc)
+    @offers = Offer.enabled.order_by_premium.order_by_remaining_time
+
+    #@offers.sort_by {|offer| offer.premium?}
+   # @offers = Offer.where.order(premium: :desc)
+
   end
 end
